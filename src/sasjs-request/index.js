a4a76408765153e5735388b77c695af266075a78
Mihajlo Medjedovic <mihajlo.kimi@gmail.com>
2020-09-21 16:09:37 +0200

    chore: cwd() fix

--- src/sasjs-request/index.js
+++ src/sasjs-request/index.js
@@ -111,7 +111,7 @@ export async function runSasJob(
         }
 
         let outputPath = path.join(
-          process.env.cwd,
+          process.env.cwd(),
           isLocal ? '/sasjsbuild' : ''
         )
 
