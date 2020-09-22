1d6c524660d9e6c2308e954b7832d5cfa97bec7b
Krishna Acondy <kc.acondy@outlook.com>
2020-09-22 12:14:12 +0100

    chore(ci): fix function

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ success_message="\e[32mSuccess!"
 failure_message="\e[91mTest \e[91mfailed."
 
 function process_result {
-  if [ $? -eq 0 ]
+  if [ $1 -eq 0 ]
   then
     echo $success_message
   else
@@ -26,11 +26,11 @@ fi
 
 echo "\e[34mSASjs \e[34mVersion"
 sasjs v
-process_result
+process_result $?
 
 echo "\e[34mSASjs \e[34mCreate"
 sasjs create test1
-process_result
+process_result $?
 
 echo "\e[34mSASjs \e[34mCreate \e[34mMinimal \e[34mApp"
 sasjs create test2 -t minimal
@@ -38,17 +38,17 @@ process_result
 
 echo "\e[34mSASjs \e[34mCreate \e[34mReact \e[34mApp"
 sasjs create test3 -t react
-process_result
+process_result $?
 
 echo "\e[34mSASjs \e[34mCreate \e[34mAngular \e[34mApp"
 sasjs create test4 -t angular
-process_result
+process_result $?
 
 echo "SASjs Create SAS Only App"
 sasjs create test5 -t sasonly
-process_result
+process_result $?
 
 echo "SASjs Compile Build"
 cd test5
 sasjs cb
-process_result
+process_result $?
