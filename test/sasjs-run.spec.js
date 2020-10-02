f205caf453f2a25f7675a4432c3d4cc267589a29
Yury Shkoda <shkoda.yuri@gmail.com>
2020-10-02 16:29:20 +0300

    fix(run): fixed regular expression in 'runSasCode' command

--- test/sasjs-run.spec.js
+++ test/sasjs-run.spec.js
@@ -4,7 +4,7 @@ import { runSasCode } from '../src/sasjs-run/index'
 describe('sasjs run', () => {
   describe('runSasCode', () => {
     it('should throw if file type is not *.sas', async () => {
-      const file = 'test.txt'
+      const file = 'test.sas.txt'
       const error = new Error(`'sasjs run' command supports only *.sas files.`)
 
       await expect(runSasCode(file, '')).rejects.toEqual(error)
