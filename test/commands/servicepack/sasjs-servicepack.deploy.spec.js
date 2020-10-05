3af778b3514b440c2fb30faf57acd830d6baae05
Yury Shkoda <shkoda.yuri@gmail.com>
2020-10-05 18:20:39 +0300

    test(servicepack-deploy): added unit tests

---
+++ test/commands/servicepack/sasjs-servicepack.deploy.spec.js
@@ -0,0 +1,51 @@
+import { servicePackDeploy } from '../../../src/sasjs-servicepack/deploy'
+import { saveGlobalRcFile } from '../../../src/utils/config-utils'
+import dotenv from 'dotenv'
+import path from 'path'
+
+describe('sasjs servicepack', () => {
+  beforeAll(() => {
+    saveGlobalRcFile(
+      JSON.stringify({
+        targets: [
+          {
+            name: 'cli-tests',
+            serverType: 'SASVIYA',
+            serverUrl: 'https://sas.analytium.co.uk',
+            appLoc: '/Public/app/cli-tests'
+          }
+        ]
+      })
+    )
+
+    process.projectDir = path.join(process.cwd())
+
+    dotenv.config()
+  })
+
+  describe('processServicepack', () => {
+    it(
+      'should deploy servicepack',
+      async () => {
+        await expect(
+          servicePackDeploy(
+            './test/commands/servicepack/testServicepack.json',
+            null,
+            true
+          )
+        ).resolves.toEqual(true)
+      },
+      60 * 1000
+    )
+
+    it(
+      'should fail because servicepack already been deployed',
+      async () => {
+        await expect(
+          servicePackDeploy('./test/commands/servicepack/testServicepack.json')
+        ).resolves.toEqual(false)
+      },
+      60 * 1000
+    )
+  })
+})
