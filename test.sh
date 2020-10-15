21c9d6786acb4a98e3bf60e3b2337e71d90cc909
Yury Shkoda <shkoda.yuri@gmail.com>
2020-10-15 14:44:51 +0300

    chore(tests): debugging

--- test.sh
+++ test.sh
@@ -24,46 +24,31 @@ else
   exit 1
 fi
 
-# echo $'\e[34mSASjs Version'
-# sasjs v
-# process_result $?
-
-# echo $'\e[34mSASjs Create'
-# sasjs create test1
-# process_result $?
-
-# echo $'\e[34mSASjs Create Minimal App'
-# sasjs create test2 -t minimal
-# process_result $?
-
-# echo $'\e[34mSASjs Create React App'
-# sasjs create test3 -t react
-# process_result $?
-
-# echo $'\e[34mSASjs Create Angular App'
-# sasjs create test4 -t angular
-# process_result $?
-
-# echo $'\e[34mSASjs Create SAS Only App'
-# sasjs create test5 -t sasonly
-# process_result $?
-
-# echo $'\e[34mSASjs Compile Build'
-# cd test5
-# sasjs cb
-# process_result $?
-
-echo $'\e[34mSASjs Compile Build Deploy alias with force flag'
-pwd
-cat ~/.sasjsrc
-cat accessToken.txt
-# sasjs create test6
-# cd test6
-# sasjs cbd -f
+echo $'\e[34mSASjs Version'
+sasjs v
 process_result $?
 
-# echo $'\e[34mSASjs Compile Build Deploy alias'
-# sasjs create test7
-# cd test7
-# sasjs cbd
-# process_result $?
+echo $'\e[34mSASjs Create'
+sasjs create test1
+process_result $?
+
+echo $'\e[34mSASjs Create Minimal App'
+sasjs create test2 -t minimal
+process_result $?
+
+echo $'\e[34mSASjs Create React App'
+sasjs create test3 -t react
+process_result $?
+
+echo $'\e[34mSASjs Create Angular App'
+sasjs create test4 -t angular
+process_result $?
+
+echo $'\e[34mSASjs Create SAS Only App'
+sasjs create test5 -t sasonly
+process_result $?
+
+echo $'\e[34mSASjs Compile Build'
+cd test5
+sasjs cb
+process_result $?
