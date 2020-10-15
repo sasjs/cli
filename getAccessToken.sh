13cb13b03016d8d2804a6177776f6df785c3baed
Yury Shkoda <shkoda.yuri@gmail.com>
2020-10-15 14:15:13 +0300

    chore(tests): debugging

--- getAccessToken.sh
+++ getAccessToken.sh
@@ -1,4 +1,4 @@
 $'\e[34mRequesting secret from Github API'
 curl \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/orgs/ORG/actions/secrets/ACCESS_TOKEN
+  https://api.github.com/orgs/sasjs/actions/secrets/ACCESS_TOKEN
