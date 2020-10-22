65ca3b9dd0cede326c9f9558f9b65a78488de98e
Saad Jutt <ihsan.distranger@gmail.com>
2020-10-22 16:46:07 +0500

    chore(lint): test folder also added to lint

--- test/commands/add/sasjs-add.spec.js
+++ test/commands/add/sasjs-add.spec.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import path from 'path'
 import { add } from '../../../src/main'
+
 import {
   getConfiguration,
   getGlobalRcFile
@@ -8,12 +9,12 @@ import {
 import { generateTimestamp } from '../../../src/utils/utils'
 
 describe('sasjs add', () => {
-  let stdin;
+  let stdin
 
   beforeAll(async () => {
     process.projectDir = path.join(process.cwd())
     dotenv.config()
-    stdin = require('mock-stdin').stdin();
+    stdin = require('mock-stdin').stdin()
   })
 
   describe('add', () => {
@@ -22,43 +23,49 @@ describe('sasjs add', () => {
       async () => {
         const timestamp = generateTimestamp()
 
-        const serverUrl= process.env.serverUrl
-        const tgtName  = `test-viya-${timestamp}`
+        const serverUrl = process.env.serverUrl
+        const tgtName = `test-viya-${timestamp}`
         const username = process.env.username
         const password = process.env.password
         const clientId = process.env.client
         const secretId = process.env.secret
         setTimeout(async () => {
-          stdin.send(['\r']);
-          stdin.send(['\r']);
-          stdin.send([`${tgtName}\r`]);
-          stdin.send(['\r']);
-          stdin.send([`${serverUrl}\r`]);
-          stdin.send([`${clientId}\r`]);
-          stdin.send([`${secretId}\r`]);
+          stdin.send(['\r'])
+          stdin.send(['\r'])
+          stdin.send([`${tgtName}\r`])
+          stdin.send(['\r'])
+          stdin.send([`${serverUrl}\r`])
+          stdin.send([`${clientId}\r`])
+          stdin.send([`${secretId}\r`])
 
-          const authCodeContent = await browserGetAuthorizationCode({ serverUrl, clientId, username, password })
+          const authCodeContent = await browserGetAuthorizationCode({
+            serverUrl,
+            clientId,
+            username,
+            password
+          })
 
-          stdin.send([`${authCodeContent}\r`]);
-          stdin.send([`1\r`]);
-        }, 1000);
+          stdin.send([`${authCodeContent}\r`])
+          stdin.send([`1\r`])
+        }, 1000)
         await expect(add()).resolves.toEqual(true)
 
-        const buildSourceFolder = require('../../../src/constants').buildSourceFolder
+        const buildSourceFolder = require('../../../src/constants')
+          .buildSourceFolder
         const config = await getConfiguration(
           path.join(buildSourceFolder, 'sasjsconfig.json')
         )
 
-        expect(config).toEqual(expect.anything());
-        expect(config.targets).toEqual(expect.anything());
+        expect(config).toEqual(expect.anything())
+        expect(config.targets).toEqual(expect.anything())
 
-        const target = config.targets.find( t => t.name === tgtName)
+        const target = config.targets.find((t) => t.name === tgtName)
 
-        expect(target).toEqual(expect.anything());
-        expect(target.tgtBuildVars.client).toEqual(clientId);
-        expect(target.tgtDeployVars.secret).toEqual(secretId);
-        expect(target.tgtBuildVars.client).toEqual(clientId);
-        expect(target.tgtDeployVars.secret).toEqual(secretId);
+        expect(target).toEqual(expect.anything())
+        expect(target.tgtBuildVars.client).toEqual(clientId)
+        expect(target.tgtBuildVars.secret).toEqual(secretId)
+        expect(target.tgtDeployVars.client).toEqual(clientId)
+        expect(target.tgtDeployVars.secret).toEqual(secretId)
       },
       60 * 1000
     )
@@ -67,39 +74,44 @@ describe('sasjs add', () => {
       async () => {
         const timestamp = generateTimestamp()
 
-        const serverUrl= process.env.serverUrl
-        const tgtName  = `test-viya-${timestamp}`
+        const serverUrl = process.env.serverUrl
+        const tgtName = `test-viya-${timestamp}`
         const username = process.env.username
         const password = process.env.password
         const clientId = process.env.client
         const secretId = process.env.secret
         setTimeout(async () => {
-          stdin.send(['2\r']);
-          stdin.send(['\r']);
-          stdin.send([`${tgtName}\r`]);
-          stdin.send(['\r']);
-          stdin.send([`${serverUrl}\r`]);
-          stdin.send([`${clientId}\r`]);
-          stdin.send([`${secretId}\r`]);
+          stdin.send(['2\r'])
+          stdin.send(['\r'])
+          stdin.send([`${tgtName}\r`])
+          stdin.send(['\r'])
+          stdin.send([`${serverUrl}\r`])
+          stdin.send([`${clientId}\r`])
+          stdin.send([`${secretId}\r`])
 
-          const authCodeContent = await browserGetAuthorizationCode({ serverUrl, clientId, username, password })
+          const authCodeContent = await browserGetAuthorizationCode({
+            serverUrl,
+            clientId,
+            username,
+            password
+          })
 
-          stdin.send([`${authCodeContent}\r`]);
-          stdin.send([`1\r`]);
-        }, 1000);
+          stdin.send([`${authCodeContent}\r`])
+          stdin.send([`1\r`])
+        }, 1000)
         await expect(add()).resolves.toEqual(true)
         const config = await getGlobalRcFile()
 
-        expect(config).toEqual(expect.anything());
-        expect(config.targets).toEqual(expect.anything());
+        expect(config).toEqual(expect.anything())
+        expect(config.targets).toEqual(expect.anything())
 
-        const target = config.targets.find( t => t.name === tgtName)
+        const target = config.targets.find((t) => t.name === tgtName)
 
-        expect(target).toEqual(expect.anything());
-        expect(target.tgtBuildVars.client).toEqual(clientId);
-        expect(target.tgtDeployVars.secret).toEqual(secretId);
-        expect(target.tgtBuildVars.client).toEqual(clientId);
-        expect(target.tgtDeployVars.secret).toEqual(secretId);
+        expect(target).toEqual(expect.anything())
+        expect(target.tgtBuildVars.client).toEqual(clientId)
+        expect(target.tgtBuildVars.secret).toEqual(secretId)
+        expect(target.tgtDeployVars.client).toEqual(clientId)
+        expect(target.tgtDeployVars.secret).toEqual(secretId)
       },
       60 * 1000
     )
--- test/helpers.js
+++ test/helpers.js
@@ -1,9 +1,16 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
-global.browserGetAuthorizationCode = async ({ serverUrl, clientId, username, password }) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(`${serverUrl}/SASLogon/oauth/authorize?client_id=${clientId}&response_type=code`);
+global.browserGetAuthorizationCode = async ({
+  serverUrl,
+  clientId,
+  username,
+  password
+}) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(
+    `${serverUrl}/SASLogon/oauth/authorize?client_id=${clientId}&response_type=code`
+  )
 
   const inputUsername = await page.waitForSelector('input[name="username"]')
   await inputUsername.click()
@@ -17,29 +24,49 @@ global.browserGetAuthorizationCode = async ({ serverUrl, clientId, username, pas
   await submitButton.click()
 
   let authHeading = await page.waitForSelector('div.infobox h3')
-  let authHeadingContent = await page.evaluate(heading => heading.innerHTML, authHeading)
-  if (authHeadingContent !== 'Authorization Code'){
-    const openidCheckbox = await page.waitForSelector('input[type="checkbox"][name="scope.0"]')
+  let authHeadingContent = await page.evaluate(
+    (heading) => heading.innerHTML,
+    authHeading
+  )
+  if (authHeadingContent !== 'Authorization Code') {
+    const openidCheckbox = await page.waitForSelector(
+      'input[type="checkbox"][name="scope.0"]'
+    )
     await openidCheckbox.click()
-    const authorizeButton = await page.waitForSelector('button[name="user_oauth_approval"]')
+    const authorizeButton = await page.waitForSelector(
+      'button[name="user_oauth_approval"]'
+    )
     await authorizeButton.click()
 
     authHeading = await page.waitForSelector('div.infobox h3')
-    authHeadingContent = await page.evaluate(heading => heading.innerHTML, authHeading)
+    authHeadingContent = await page.evaluate(
+      (heading) => heading.innerHTML,
+      authHeading
+    )
   }
   expect(authHeadingContent).toBe('Authorization Code')
 
   const authMessage = await page.waitForSelector('div.infobox p')
-  const authMessageContent = await page.evaluate(message => message.innerHTML, authMessage)
-  expect(authMessageContent).toBe('Copy the following code, and paste it in your application:')
+  const authMessageContent = await page.evaluate(
+    (message) => message.innerHTML,
+    authMessage
+  )
+  expect(authMessageContent).toBe(
+    'Copy the following code, and paste it in your application:'
+  )
 
   const authCode = await page.waitForSelector('div.infobox h4')
-  const authCodeContent = await page.evaluate(message => message.innerHTML, authCode)
+  const authCodeContent = await page.evaluate(
+    (message) => message.innerHTML,
+    authCode
+  )
 
-  const signOutButton = await page.waitForSelector('div.infobox button[type="button"]')
+  const signOutButton = await page.waitForSelector(
+    'div.infobox button[type="button"]'
+  )
   await signOutButton.click()
   setTimeout(async () => {
-    await browser.close();
-  }, 1000);
+    await browser.close()
+  }, 1000)
   return authCodeContent
 }
