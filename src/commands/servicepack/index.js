26c8edcce1a41c28a74a93393190899a846ebfed
Yury Shkoda <shkoda.yuri@gmail.com>
2020-11-05 12:06:17 +0300

    refactor(sasjs-servicepack): used command utility

--- src/commands/servicepack/index.js
+++ src/commands/servicepack/index.js
@@ -1,23 +1,20 @@
 import { servicePackDeploy } from './deploy'
-import {
-  getCommandParameter,
-  getCommandParameterLastMultiWord,
-  isFlagPresent
-} from '../../utils/command-utils'
+import { Command } from '../../utils/command'
 
 import chalk from 'chalk'
 
 export async function processServicepack(commandLine) {
-  const command = commandLine[1]
-  const commands = {
+  const command = new Command(commandLine)
+  const subCommand = command.getSubCommand()
+  const subCommands = {
     deploy: 'deploy'
   }
 
-  if (!commands.hasOwnProperty(command)) {
+  if (!subCommands.hasOwnProperty(subCommand)) {
     console.log(
       chalk.redBright(
         `Not supported servicepack command. Supported commands are:\n${Object.keys(
-          commands
+          subCommands
         ).join('\n')}`
       )
     )
@@ -25,27 +22,14 @@ export async function processServicepack(commandLine) {
     return
   }
 
-  const commandExample =
-    'sasjs servicepack <command> --source ../viyadeploy.json --target targetName'
+  const targetName = command.getFlagValue('target')
+  const jsonFilePath = command.getFlagValue('source')
+  const isForced = command.getFlagValue('force')
 
   let output
 
-  switch (command) {
-    case commands.deploy:
-      const targetName = getCommandParameterLastMultiWord(
-        '-t',
-        '--target',
-        commandLine,
-        commandExample
-      )
-      const jsonFilePath = getCommandParameter(
-        '-s',
-        '--source',
-        commandLine,
-        commandExample
-      )
-      const isForced = isFlagPresent('-f', commandLine)
-
+  switch (subCommand) {
+    case subCommands.deploy:
       output = await servicePackDeploy(jsonFilePath, targetName, isForced)
 
       break
