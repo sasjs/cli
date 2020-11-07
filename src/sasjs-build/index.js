674d6b64701a0708a0547edc9f4a7ec4f26c486b
Mihajlo Medjedovic <mihajlo.kimi@gmail.com>
2020-11-07 17:59:30 +0100

    docs: jsdoc comments added

--- src/sasjs-build/index.js
+++ src/sasjs-build/index.js
@@ -335,6 +335,13 @@ function getWebServiceScriptInvocation(serverType) {
   }
 }
 
+/**
+ * Folders inside of `SASJS` folder are converted to JSON structure.
+ * That JSON file is used to deploy services and jobs.
+ * Services are deployed as direct subfolders within the appLoc.
+ * Jobs are deployed within a jobs folder within the appLoc.
+ * @param {Server type as a string, either VIYA or SAS9} serverType 
+ */
 async function getFolderContent(serverType) {
   const buildSubFolders = await getSubFoldersInFolder(buildDestinationFolder)
 
