fda2515580672eb6a9c015fcda6fb9b45fada8c6
Yury Shkoda <shkoda.yuri@gmail.com>
2020-11-09 14:27:10 +0300

    test(compile): added unit tests for 'loadDependencies.spec'

--- src/sasjs-build/index.js
+++ src/sasjs-build/index.js
@@ -1,3 +1,5 @@
+import * as thisModule from './index'
+
 import find from 'find'
 import path from 'path'
 import chalk from 'chalk'
@@ -525,17 +527,17 @@ export async function loadDependencies(
   if (type === 'service') {
     serviceVars = await getServiceVars()
 
-    init = await getServiceInit()
+    init = await thisModule.getServiceInit()
 
-    term = await getServiceTerm()
+    term = await thisModule.getServiceTerm()
 
     fileContent = fileContent
       ? `\n* Service start;\n${fileContent}\n* Service end;`
       : ''
   } else {
-    init = await getJobInit()
+    init = await thisModule.getJobInit()
 
-    term = await getJobTerm()
+    term = await thisModule.getJobTerm()
 
     fileContent = fileContent
       ? `\n* Job start;\n${fileContent}\n* Job end;`
@@ -580,18 +582,18 @@ export async function getServiceInit() {
   return init ? `\n* ServiceInit start;\n${init}\n* ServiceInit end;` : ''
 }
 
-async function getJobInit() {
+export async function getJobInit() {
   const init = (await getTargetSpecificFile('jobInit', targetToBuild)).content
   return init ? `\n* JobInit start;\n${init}\n* JobInit end;` : ''
 }
 
-async function getServiceTerm() {
+export async function getServiceTerm() {
   const term = (await getTargetSpecificFile('ServiceTerm', targetToBuild))
     .content
   return term ? `\n* ServiceTerm start;\n${term}\n* ServiceTerm end;` : ''
 }
 
-async function getJobTerm() {
+export async function getJobTerm() {
   const term = (await getTargetSpecificFile('jobTerm', targetToBuild)).content
   return term ? `\n* JobTerm start;\n${term}\n* JobTerm end;` : ''
 }
--- test/commands/compile/loadDependencies.spec.js
+++ test/commands/compile/loadDependencies.spec.js
@@ -1,34 +1,59 @@
 import path from 'path'
-jest.unmock('../../../src/sasjs-build/index')
-import { loadDependencies } from '../../../src/sasjs-build/index'
+import * as compileModule from '../../../src/sasjs-build/index'
 
 process.projectDir = path.join(process.cwd())
 describe('loadDependencies', () => {
-  test.todo('it should load dependencies for a service', async (done) => {
-    const dependencies = await loadDependencies(
+  test('it should load dependencies for a service', async (done) => {
+    spyOn(compileModule, 'getServiceInit').and.returnValue(
+      '\n* ServiceInit start;\nFAKE SERVICE INIT\n* ServiceInit end;'
+    )
+    spyOn(compileModule, 'getServiceTerm').and.returnValue(
+      '\n* ServiceTerm start;\nFAKE SERVICE TERM\n* ServiceTerm end;'
+    )
+
+    const dependencies = await compileModule.loadDependencies(
       path.join(__dirname, './service.sas'),
       [],
       [],
       'service'
     )
 
-    console.log(dependencies)
-    // TODO: Assert on the fact that dependencies text is included
-    expect(true).toEqual(true)
+    expect(
+      /\* ServiceInit start;\nFAKE SERVICE INIT\n\* ServiceInit end;/.test(
+        dependencies
+      )
+    ).toEqual(true)
+    expect(
+      /\* ServiceTerm start;\nFAKE SERVICE TERM\n\* ServiceTerm end;/.test(
+        dependencies
+      )
+    ).toEqual(true)
+
     done()
   })
 
-  test.todo('it should load dependencies for a job', async (done) => {
-    const dependencies = await sasjsBuild.loadDependencies(
+  test('it should load dependencies for a job', async (done) => {
+    spyOn(compileModule, 'getJobTerm').and.returnValue(
+      '\n* JobTerm start;\nFAKE JOB TERM\n* JobTerm end;'
+    )
+    spyOn(compileModule, 'getJobInit').and.returnValue(
+      '\n* JobInit start;\nFAKE JOB INIT\n* JobInit end;'
+    )
+
+    const dependencies = await compileModule.loadDependencies(
       path.join(__dirname, './service.sas'),
       [],
       [],
       'job'
     )
 
-    console.log(dependencies)
-    // TODO: Assert on the fact that dependencies text is included
-    expect(true).toEqual(true)
+    expect(
+      /\* JobTerm start;\nFAKE JOB TERM\n\* JobTerm end;/.test(dependencies)
+    ).toEqual(true)
+    expect(
+      /\* JobInit start;\nFAKE JOB INIT\n\* JobInit end;/.test(dependencies)
+    ).toEqual(true)
+
     done()
   })
 })
