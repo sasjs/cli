2f7e2ec9e6ffcf628ce02cb201e1944b8161bfd1
Krishna Acondy <kc.acondy@outlook.com>
2020-11-13 16:10:17 +0000

    fix(line-endings): handle CRLF line endings when removing comments from a file

--- test/removeComments.spec.js
+++ test/removeComments.spec.js
@@ -9,6 +9,7 @@ describe('removeComments', () => {
 
   test('should not remove single line block comment', () => {
     const text = `/* test */`
+
     expect(removeComments(text)).toEqual(text)
   })
 
@@ -22,11 +23,41 @@ describe('removeComments', () => {
 
   test('should keep inline comments intact when they start a line', () => {
     const text = '/* Some Comment */ CODE HERE;'
+
     expect(removeComments(text)).toEqual(text)
   })
 
   test('should keep inline comments intact when they are within a line', () => {
     const text = 'CODE HERE /* Some Comment  */'
+
+    expect(removeComments(text)).toEqual(text)
+  })
+
+  test('should handle CRLF line endings', () => {
+    const text = 'CODE HERE\r\n\r\nMORE CODE'
+    const expectedText = 'CODE HERE\nMORE CODE'
+
+    expect(removeComments(text)).toEqual(expectedText)
+  })
+
+  test('should handle a mix of CRLF and LF line endings', () => {
+    const text = 'CODE HERE\r\n\r\nMORE CODE\nEVEN MORE CODE'
+    const expectedText = 'CODE HERE\nMORE CODE\nEVEN MORE CODE'
+
+    expect(removeComments(text)).toEqual(expectedText)
+  })
+
+  test('should return an empty string when the input is falsy', () => {
+    expect(removeComments(null)).toEqual('')
+    expect(removeComments(undefined)).toEqual('')
+    expect(removeComments('')).toEqual('')
+  })
+
+  test('should preserve formatting when removing comments', () => {
+    const text = `if (this === 'code') {
+  console.log('Pigs can fly.')
+  }`
+
     expect(removeComments(text)).toEqual(text)
   })
 })
