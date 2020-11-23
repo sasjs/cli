061668d5aa7050218bf24448302f1cfc3545c7c9
Mihajlo Medjedovic <mihajlo.kimi@gmail.com>
2020-11-23 10:55:36 +0100

    fix: deploy getting access_token

--- src/commands/deploy.js
+++ src/commands/deploy.js
@@ -142,7 +142,7 @@ async function getSASjsAndAccessToken(buildTarget) {
     serverType: buildTarget.serverType
   })
 
-  const accessToken = await getToken(buildTarget, sasjs)
+  const accessToken = await getAccessToken(buildTarget, sasjs)
   return {
     sasjs,
     accessToken
@@ -336,28 +336,4 @@ async function deployToSas9(
   } else {
     console.error(chalk.redBright('Unable to create log file.'))
   }
-}
-
-async function getToken(buildTarget, sasjsInstance) {
-  const clientId = await getVariable('client', buildTarget)
-  const clientSecret = await getVariable('secret', buildTarget)
-  if (!clientId) {
-    throw new Error(
-      'A client ID is required for SAS Viya deployments.\n Please ensure that `client` is present in your build target configuration or in your .env file, and try again.\n'
-    )
-  }
-  if (!clientSecret) {
-    throw new Error(
-      'A client secret is required for SAS Viya deployments.\n Please ensure that `secret` is present in your build target configuration or in your .env file, and try again.\n'
-    )
-  }
-
-  const accessToken = await getAccessToken(
-    sasjsInstance,
-    clientId,
-    clientSecret,
-    buildTarget,
-    true
-  )
-  return accessToken
 }
