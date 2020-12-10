93057215ed8866d935ff9d8e538062fff65519d6
Yury Shkoda <shkoda.yuri@gmail.com>
2020-12-10 16:28:53 +0300

    chore(job-execute): rolled back to original state

--- test.sh
+++ test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-# process_result() {
-#   success_message=$'\e[32mSuccess!'
-#   failure_message=$'\e[91mTest failed.'
+process_result() {
+  success_message=$'\e[32mSuccess!'
+  failure_message=$'\e[91mTest failed.'
 
-#   if [ $1 -eq 0 ]
-#   then
-#     echo $success_message
-#   else
-#     echo $failure_message
-#     exit 1
-#   fi
-# }
+  if [ $1 -eq 0 ]
+  then
+    echo $success_message
+  else
+    echo $failure_message
+    exit 1
+  fi
+}
 
 echo $'\e[34mLinking SASjs CLI...'
 npm link
@@ -24,57 +24,31 @@ else
   exit 1
 fi
 
-# echo $'\e[34mSASjs Version'
-# sasjs v
-# process_result $?
+echo $'\e[34mSASjs Version'
+sasjs v
+process_result $?
 
 echo $'\e[34mSASjs Create'
 sasjs create test1
-# process_result $?
+process_result $?
 
-# echo $'\e[34mSASjs Create Minimal App'
-# sasjs create test2 -t minimal
-# process_result $?
+echo $'\e[34mSASjs Create Minimal App'
+sasjs create test2 -t minimal
+process_result $?
 
-# echo $'\e[34mSASjs Create React App'
-# sasjs create test3 -t react
-# process_result $?
+echo $'\e[34mSASjs Create React App'
+sasjs create test3 -t react
+process_result $?
 
-# echo $'\e[34mSASjs Create Angular App'
-# sasjs create test4 -t angular
-# process_result $?
+echo $'\e[34mSASjs Create Angular App'
+sasjs create test4 -t angular
+process_result $?
 
-# echo $'\e[34mSASjs Create SAS Only App'
-# sasjs create test5 -t sasonly
-# process_result $?
+echo $'\e[34mSASjs Create SAS Only App'
+sasjs create test5 -t sasonly
+process_result $?
 
-# echo $'\e[34mSASjs Compile Build'
-# cd test5
-# sasjs cb
-# process_result $?
-
-pwd
-ls -a
-cd
-pwd
-ls -a
-cat .sasjsrc
-
-echo $'\e[34mSASjs Job Execute Test'
-cd test1
-sasjs job execute testJob/job -t cli-tests-job --wait --returnStatusOnly
-if [ $? -eq 0 ]
-then
-  echo $'\e[32mSuccess: test passed.'
-else
-  echo $'\e[91mfailure: test failed.'
-  exit 1
-fi
-sasjs job execute testJob/jobWithWarning -t cli-tests-job --wait --returnStatusOnly
-if [ $? -eq 1 ]
-then
-  echo $'\e[32mSuccess: test passed.'
-else
-  echo $'\e[91mfailure: test failed.'
-  exit 1
-fi
+echo $'\e[34mSASjs Compile Build'
+cd test5
+sasjs cb
+process_result $?
