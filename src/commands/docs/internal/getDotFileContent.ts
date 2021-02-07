f7092c524f40581cf2a9db72f04a9d56ece30a43
Saad Jutt <ihsan.distranger@gmail.com>
2021-02-07 18:04:47 +0500

    fix(lineage): Added links to librefs

--- src/commands/docs/internal/getDotFileContent.ts
+++ src/commands/docs/internal/getDotFileContent.ts
@@ -7,10 +7,35 @@ import {
 } from '../../../utils/file'
 import { asyncForEach } from '../../../utils/utils'
 
+const CRAYONS = [
+  '#e6194b', // red
+  '#3cb44b', // green
+  '#4363d8', // blue
+  '#f58231', // orange
+  '#911eb4', // purple
+  '#46f0f0', // cyan
+  '#f032e6', // magenta
+  '#bcf60c', // lime
+  '#fabebe', // pink
+  '#008080', // teal
+  '#e6beff', // lavender
+  '#9a6324', // brown
+  '#fffac8', // beige
+  '#800000', // maroon
+  '#aaffc3', // mint
+  '#808000', // olive
+  '#ffd8b1', // apricot
+  '#000075', // navy
+  '#808080', // gray
+  '#ffe119', // yellow
+  '#ffffff' // white
+]
+
 export async function getDotFileContent(folderList: string[]): Promise<string> {
   let nodeDictionary = new Map()
   let fileNodes = new Map()
   let paramNodes = new Map()
+  let paramNodeTyes = new Map()
 
   // Populating both Maps
   await asyncForEach(folderList, async (folder) => {
@@ -42,6 +67,10 @@ export async function getDotFileContent(folderList: string[]): Promise<string> {
           })
         if (!nodeDictionary.has(inputParam))
           nodeDictionary.set(inputParam, `n${nodeDictionary.size}`)
+
+        const librefFound = inputParam.match(/^[A-Z]{2,5}\./)
+        if (librefFound && !paramNodeTyes.has(librefFound[0]))
+          paramNodeTyes.set(librefFound[0], CRAYONS[paramNodeTyes.size])
       })
 
       const fileOutputs = getList('<h4> Data Outputs </h4>', fileContent)
@@ -55,7 +84,13 @@ export async function getDotFileContent(folderList: string[]): Promise<string> {
           })
         if (!nodeDictionary.has(outputParam))
           nodeDictionary.set(outputParam, `n${nodeDictionary.size}`)
+
+        const librefFound = outputParam.match(/^[A-Z]{2,5}\./)
+        if (librefFound && !paramNodeTyes.has(librefFound[0]))
+          paramNodeTyes.set(librefFound[0], CRAYONS[paramNodeTyes.size])
       })
+
+      if (fileInputs.length === 0 && fileOutputs.length === 0) return
 
       fileNodes.set(fileName, {
         edges: fileOutputs,
@@ -72,9 +107,12 @@ export async function getDotFileContent(folderList: string[]): Promise<string> {
 
   // Generating Nodes for Dot fileContent
   paramNodes.forEach((node, key) => {
+    let color = '#A3D0D4'
+    const librefFound = key.match(/^[A-Z]{2,5}\./)
+    if (librefFound) color = paramNodeTyes.get(librefFound[0])
     dotNodes += `${nodeDictionary.get(key)} [label="${
       node.label
-    }" shape="cylinder" style="filled" color="#A3D0D4"]\n`
+    }" shape="cylinder" style="filled" color="${color}"]\n`
     if (node.edges.length)
       dotVertices += `${nodeDictionary.get(
         key
