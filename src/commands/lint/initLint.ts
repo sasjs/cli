c000b2339b2bbadde428a09f134e661244684b6c
Saad Jutt <ihsan.distranger@gmail.com>
2021-04-14 19:46:51 +0500

    chore(lint): TSDoc comment added

--- src/commands/lint/initLint.ts
+++ src/commands/lint/initLint.ts
@@ -1,5 +1,8 @@
 import { createLintConfigFile } from '../shared/createLintConfigFile'
 
+/**
+ * Initialises Lint Confiugartion in current sasjs application
+ */
 export async function initLint() {
   const parentFolderName = '.'
   await createLintConfigFile(parentFolderName)
