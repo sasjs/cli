cee353dcdbe0c21c8f6d871aa9c79216756bd2ac
Saad Jutt <ihsan.distranger@gmail.com>
2021-05-12 14:38:38 +0500

    fix: removed extra check for targetName

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,6 @@
 import SASjs from '@sasjs/adapter/node'
 import { getConstants } from '../constants'
 import { Configuration, Target, TargetJson } from '@sasjs/utils/types'
-import { validateTargetName } from '@sasjs/utils/types/targetValidators'
 import { readFile, folderExists, createFile, fileExists } from './file'
 import { isAccessTokenExpiring, getNewAccessToken, refreshTokens } from './auth'
 import path from 'path'
@@ -47,8 +46,6 @@ export async function findTargetInConfiguration(
   targetName: string,
   targetScope?: TargetScope
 ): Promise<{ target: Target; isLocal: boolean }> {
-  if (targetName) validateTargetName(targetName)
-
   const rootDir = await getProjectRoot()
 
   if (rootDir !== process.projectDir) {
