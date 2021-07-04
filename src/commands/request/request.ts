bc31ad40b2cb877d7ac318c318c8b313bc4b4f15
Mihajlo <mihajlo.medjedovic@outlook.com>
2021-07-04 12:30:26 +0200

    fix: request command including context from target

--- src/commands/request/request.ts
+++ src/commands/request/request.ts
@@ -73,6 +73,7 @@ export async function runSasJob(command: Command) {
     allowInsecureRequests: target.allowInsecureRequests,
     appLoc: target.appLoc,
     serverType: target.serverType,
+    contextName: target.contextName,
     useComputeApi: false
   })
 
