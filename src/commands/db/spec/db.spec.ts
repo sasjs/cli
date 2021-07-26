92c84e0e7ed10c2d89e678550a6f081526fab321
Saad Jutt <ihsan.distranger@gmail.com>
2021-07-26 16:05:37 +0500

    test(db): re-written failing db test correctly

--- src/commands/db/spec/db.spec.ts
+++ src/commands/db/spec/db.spec.ts
@@ -1,17 +1,18 @@
 import path from 'path'
 import { createTestApp, removeTestApp, verifyStep } from '../../../utils/test'
-import { generateTimestamp } from '@sasjs/utils'
+import { deleteFolder, generateTimestamp } from '@sasjs/utils'
 import { buildDB } from '../db'
 
 describe(`sasjs db`, () => {
-  const timestamp = generateTimestamp()
-  const appName = `test-app-DB-${timestamp}`
+  let appName: string
 
-  beforeAll(async () => {
+  beforeEach(async () => {
+    const timestamp = generateTimestamp()
+    appName = `test-app-DB-${timestamp}`
     await createTestApp(__dirname, appName)
   })
 
-  afterAll(async () => {
+  afterEach(async () => {
     await removeTestApp(__dirname, appName)
   })
 
@@ -19,5 +20,11 @@ describe(`sasjs db`, () => {
     await expect(buildDB()).toResolve()
 
     await verifyStep('db')
+  })
+
+  it(`should throw an error when the db folder does not exist`, async () => {
+    await deleteFolder(path.join(__dirname, appName, 'sasjs', 'db'))
+
+    await expect(buildDB()).rejects.toThrow('no such file or directory')
   })
 })
