003c04def31b6c1d3b04ce8c3760f14cbf3a22fd
Saad Jutt <ihsan.distranger@gmail.com>
2021-07-31 04:14:19 +0500

    test(base64): fixed for windows CRLF

--- src/commands/web/spec/updateTagSource.spec.ts
+++ src/commands/web/spec/updateTagSource.spec.ts
@@ -12,6 +12,7 @@ import {
   Target
 } from '@sasjs/utils'
 import { updateTagSource } from '..'
+import { isWindows } from '../../../utils/command'
 
 describe('updateTagSource', () => {
   let destinationPath: string
@@ -36,9 +37,11 @@ describe('updateTagSource', () => {
     const target = { serverType: ServerType.Sas9 } as any as Target
     const assetPathMap = []
 
+    const endlineBase64 = isWindows() ? '0K' : 'o='
+
     const script1Filename = 'script1.js'
     const script1SASProgramName = 'script1-js.sas'
-    const script1Base64 = `O1sn4pWQJywgJ+KVpCcsICfilZQnLCAn4pWXJywgJ+KVkCcsICfilacnLCAn4pWaJywgJ+KVnScsICfilZEnLCAn4pWfJywgJ+KUgCcsICfilLwnLCAn4pWRJywgJ+KVoicsICfilIInXQo=`
+    const script1Base64 = `O1sn4pWQJywgJ+KVpCcsICfilZQnLCAn4pWXJywgJ+KVkCcsICfilacnLCAn4pWaJywgJ+KVnScsICfilZEnLCAn4pWfJywgJ+KUgCcsICfilLwnLCAn4pWRJywgJ+KVoicsICfilIInXQ${endlineBase64}`
 
     scriptTag.setAttribute('src', script1Filename)
     const sasProgramScript1Path = path.join(
@@ -64,7 +67,7 @@ describe('updateTagSource', () => {
 
     const script2Filename = './script2.js'
     const script2SASProgramName = 'script2-js.sas'
-    const script2Base64 = `O1snxKYnLCAnxpUnLCAn0qInLCAn0ronLCAn04cnLCAn1IonXQo=`
+    const script2Base64 = `O1snxKYnLCAnxpUnLCAn0qInLCAn0ronLCAn04cnLCAn1IonXQ${endlineBase64}`
     scriptTag.setAttribute('src', script2Filename)
     const sasProgramScript2Path = path.join(
       destinationPath,
