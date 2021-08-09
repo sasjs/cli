30cfae5ec8fa7943096bffce729f38361e24b23b
Saad Jutt <ihsan.distranger@gmail.com>
2021-08-10 01:49:19 +0500

    chore: build fix

--- src/commands/deploy/compileBuildDeployCommand.ts
+++ src/commands/deploy/compileBuildDeployCommand.ts
@@ -1,4 +1,3 @@
-import { getConstants } from '../../constants'
 import { CommandExample, ReturnCode } from '../../types/command'
 import { TargetCommand } from '../../types/command/targetCommand'
 import { compile } from '../compile/compile'
@@ -33,7 +32,7 @@ export class CompileBuildDeployCommand extends TargetCommand {
       return returnCode
     }
 
-    const { buildDestinationFolder } = await getConstants()
+    const { buildDestinationFolder } = process.sasjsConstants
 
     process.logger?.success(
       `Services have been successfully compiled!\nThe compile output is located in the ${buildDestinationFolder} directory.`
