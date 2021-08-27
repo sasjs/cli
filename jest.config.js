7918512db995d43cf4e7308567078b28bdf57ec8
Krishna Acondy <kc.acondy@outlook.com>
2021-08-27 08:54:11 +0100

    fix(jsdom): set up jest for jsdom

--- jest.config.js
+++ jest.config.js
@@ -124,7 +124,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: [],
+  setupFiles: ['./jest.env.js'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['jest-extended'],
