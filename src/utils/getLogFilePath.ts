31b4f07ba4c0be85701ddc3e94adb919afab896e
Yury Shkoda <shkoda.yuri@gmail.com>
2022-01-27 10:58:07 +0300

    fix(utils): fix path.sep in getLogFilePath utility

--- src/utils/getLogFilePath.ts
+++ src/utils/getLogFilePath.ts
@@ -1,16 +1,17 @@
 import path from 'path'
 
 export const getLogFilePath = (logArg: unknown, jobPath: string) => {
-  if (logArg === undefined || !jobPath || jobPath === '') {
-    return undefined
-  }
+  if (logArg === undefined || !jobPath || jobPath === '') return undefined
 
   if (logArg) {
     const currentDirPath = path.isAbsolute(logArg as string)
       ? ''
       : process.projectDir
+
     return path.join(currentDirPath, logArg as string)
   }
-  const logFileName = `${jobPath.split('/').slice(-1).pop()}.log`
+
+  const logFileName = `${jobPath.split(path.sep).slice(-1).pop()}.log`
+
   return path.join(process.projectDir, logFileName)
 }
