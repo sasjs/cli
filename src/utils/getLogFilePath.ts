62b3e1dff9345186c889170ca78c65542c705fc2
Yury Shkoda <shkoda.yuri@gmail.com>
2022-01-27 17:47:44 +0300

    fix(utils): fix getLogFilePath

--- src/utils/getLogFilePath.ts
+++ src/utils/getLogFilePath.ts
@@ -11,7 +11,7 @@ export const getLogFilePath = (logArg: unknown, jobPath: string) => {
     return path.join(currentDirPath, logArg as string)
   }
 
-  const logFileName = `${jobPath.split(path.sep).slice(-1).pop()}.log`
+  const logFileName = `${jobPath.split('/').slice(-1).pop()}.log`
 
   return path.join(process.projectDir, logFileName)
 }
