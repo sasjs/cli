bc4c4b93533b3ba0e3aac48af0ac53e247a4a831
Sabir Hassan <sabirbk06@gmail.com>
2022-03-21 01:07:18 +0500

    chore: update help docs

--- src/commands/help/help.ts
+++ src/commands/help/help.ts
@@ -239,7 +239,7 @@ export async function printHelpText() {
         `The target can exist either in the local project configuration or in the global .sasjsrc file.`,
         `<sasProgramPath> - if this has a leading slash (eg /Public/app/folder/servicename) then it must be the full path. If it is a relative path (eg path/servicename) then it will be pre-pended with the appLoc - which must then be defined in the sasjs config.`,
         `<path/to/log> - Location in which to store the log file. If not provided AND current directory is a sasjs project, it will be saved in sasjsresults else in the current directory`,
-        `<path/to/output> - Location to store the output file. If not provided AND current directory is a sasjs project, a file named output.json will be saved in the sasjsresults folder else in current directory.`
+        `<path/to/output> - Location to store the output file. If not provided AND current directory is a sasjs project, an output file  will be saved in the sasjsresults folder else in current directory.`
       ]
     },
     {
