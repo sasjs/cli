a0f20a103681374cf55646921c2c1037d867206b
Saad Jutt <ihsan.distranger@gmail.com>
2022-07-03 03:20:14 +0500

    test(cbd): updated for minimal-seed-app

--- src/commands/deploy/spec/cbd.spec.ts
+++ src/commands/deploy/spec/cbd.spec.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import SASjs from '@sasjs/adapter/node'
 import { encode } from 'js-base64'
 import {
@@ -83,6 +84,23 @@ describe('sasjs cbd with server type SASJS', () => {
       '    )' +
       '  %mend;\n'
 
+    const mocksPath = path.join(__dirname, appName, 'sasjs', 'mocks')
+    const appinitJSPath = path.join(
+      mocksPath,
+      'services',
+      'common',
+      'appinit.js'
+    )
+    const getdataJSPath = path.join(
+      mocksPath,
+      'services',
+      'common',
+      'getdata.js'
+    )
+
+    const appinitJSContents = await readFile(appinitJSPath)
+    const getdataJSContents = await readFile(getdataJSPath)
+
     expect(sasjs.deployToSASjs).toHaveBeenCalledWith(
       {
         appLoc: target.appLoc,
@@ -99,9 +117,7 @@ describe('sasjs cbd with server type SASJS', () => {
                     {
                       name: 'appinit.js',
                       type: 'file',
-                      code: encode(
-                        `// mock a constant in the pre-existing _webout variable\n_webout = \`\n{"SYSDATE":"21JUN22","SYSTIME":"16:46","areas":[{"AREA":"Adak"},{"AREA":"Adel"},{"AREA":"Afognak"},{"AREA":"Ajo"},{"AREA":"Albany"},{"AREA":"Albuquerque"},{"AREA":"Alturas"},{"AREA":"Ashton"},{"AREA":"Atka"},{"AREA":"Atlanta"},{"AREA":"Attu"},{"AREA":"Aztec"},{"AREA":"Baker"},{"AREA":"Bakersfield"},{"AREA":"Baltimore"},{"AREA":"Beaver"},{"AREA":"Bend"},{"AREA":"Bendeleben"},{"AREA":"Bering Glacier"},{"AREA":"Bettles"},{"AREA":"Bluefield"},{"AREA":"Boise"},{"AREA":"Bozeman"},{"AREA":"Bradfield Cana"},{"AREA":"Brigham City"},{"AREA":"Burns"},{"AREA":"Butte"},{"AREA":"Caliente"},{"AREA":"Candle"},{"AREA":"Canyon City"},{"AREA":"Carlsbad"},{"AREA":"Casper"},{"AREA":"Cedar City"},{"AREA":"Challis"},{"AREA":"Charley River"},{"AREA":"Charlottesvill"},{"AREA":"Chico"},{"AREA":"Chignik"},{"AREA":"Choteau"},{"AREA":"Circle"},{"AREA":"Clifton"},{"AREA":"Cody"},{"AREA":"Cold Bay"},{"AREA":"Concrete"},{"AREA":"Cortez"},{"AREA":"Craig"},{"AREA":"Crescent"},{"AREA":"Cumberland"},{"AREA":"Death Valley"},{"AREA":"Delta"},{"AREA":"Denver"},{"AREA":"Dillon"},{"AREA":"Douglas"},{"AREA":"Driggs"},{"AREA":"Dubois"},{"AREA":"Durango"},{"AREA":"El Centro"},{"AREA":"Elk City"},{"AREA":"Elko"},{"AREA":"Ely"},{"AREA":"Emory Peak"},{"AREA":"Escalante"},{"AREA":"False Pass"},{"AREA":"Fresno"},{"AREA":"Gareloi Island"},{"AREA":"Goldfield"},{"AREA":"Grand Canyon"},{"AREA":"Grangeville"},{"AREA":"Gulkana"},{"AREA":"Hailey"},{"AREA":"Hamilton"},{"AREA":"Hawaii"},{"AREA":"Holbrook"},{"AREA":"Hoquiam"},{"AREA":"Hot Springs"},{"AREA":"Hughes"},{"AREA":"Idaho Falls"},{"AREA":"Iditarod"},{"AREA":"Jordan Valley"},{"AREA":"Ketchikan"},{"AREA":"Kingman"},{"AREA":"Klamath Falls"},{"AREA":"Knoxville"},{"AREA":"Lander"},{"AREA":"Las Cruces"},{"AREA":"Las Vegas"},{"AREA":"Leadville"},{"AREA":"Lewistown"},{"AREA":"Little Rock"},{"AREA":"Livengood"},{"AREA":"Long Beach"},{"AREA":"Los Angeles"},{"AREA":"Lovelock"},{"AREA":"Lukeville"},{"AREA":"Lund"},{"AREA":"Marble Canyon"},{"AREA":"Marfa"},{"AREA":"Mariposa"},{"AREA":"Mcdermitt"},{"AREA":"Medford"},{"AREA":"Melozitna"},{"AREA":"Mesa"},{"AREA":"Millett"},{"AREA":"Moab"},{"AREA":"Montrose"},{"AREA":"Mt. Fairweathe"},{"AREA":"Mt. Katmai"},{"AREA":"Mt. Mckinley"},{"AREA":"Mt. Michelson"},{"AREA":"Needles"},{"AREA":"Nogales"},{"AREA":"Nulato"},{"AREA":"Ogden"},{"AREA":"Okanogan"},{"AREA":"Pendleton"},{"AREA":"Petersburg"},{"AREA":"Phenix City"},{"AREA":"Phoenix"},{"AREA":"Phoenix South"},{"AREA":"Pocatello"},{"AREA":"Poplar Bluff"},{"AREA":"Port Alexander"},{"AREA":"Port Moller"},{"AREA":"Prescott"},{"AREA":"Presidio"},{"AREA":"Preston"},{"AREA":"Price"},{"AREA":"Pueblo"},{"AREA":"Rat Islands"},{"AREA":"Raton"},{"AREA":"Rawlins"},{"AREA":"Redding"},{"AREA":"Reno"},{"AREA":"Richfield"},{"AREA":"Roanoke"},{"AREA":"Roseburg"},{"AREA":"Roundup"},{"AREA":"Ruby"},{"AREA":"Russian Missio"},{"AREA":"Sacramento"},{"AREA":"Saint Johns"},{"AREA":"Salem"},{"AREA":"Salina"},{"AREA":"Salt Lake City"},{"AREA":"Salton Sea"},{"AREA":"Samalga Island"},{"AREA":"San Bernardino"},{"AREA":"San Diego"},{"AREA":"San Francisco"},{"AREA":"San Jose"},{"AREA":"San Luis Obisp"},{"AREA":"Santa Ana"},{"AREA":"Santa Cruz"},{"AREA":"Santa Fe"},{"AREA":"Santa Maria"},{"AREA":"Santa Rosa"},{"AREA":"Seattle"},{"AREA":"Seguam"},{"AREA":"Shungnak"},{"AREA":"Silver City"},{"AREA":"Sitka"},{"AREA":"Sleetmute"},{"AREA":"Socorro"},{"AREA":"Solomon"},{"AREA":"Survey Pass"},{"AREA":"Susanville"},{"AREA":"Tampa"},{"AREA":"Tanana"},{"AREA":"The Dalles"},{"AREA":"Thermopolis"},{"AREA":"Tonopah"},{"AREA":"Tooele"},{"AREA":"Torrington"},{"AREA":"Trinidad"},{"AREA":"Trona"},{"AREA":"Tucson"},{"AREA":"Tularosa"},{"AREA":"Twin Falls"},{"AREA":"Tyonek"},{"AREA":"Ugashik"},{"AREA":"Ukiah"},{"AREA":"Umnak"},{"AREA":"Unalaska"},{"AREA":"Unimak"},{"AREA":"Vancouver"},{"AREA":"Vernal"},{"AREA":"Vya"},{"AREA":"Walker Lake"},{"AREA":"Walla Walla"},{"AREA":"Wallace"},{"AREA":"Weed"},{"AREA":"Wells"},{"AREA":"Wenatchee"},{"AREA":"White Sulphur"},{"AREA":"Williams"},{"AREA":"Winnemucca"},{"AREA":"Yakima"}],\n"SYSUSERID":"sasjs"\n,"MF_GETUSER":"sasjs","_DEBUG":"",\n"_PROGRAM":"/Public/app/minimal-seed-app/services/common/appinit",\n"SYSCC":"0",\n"SYSERRORTEXT":"",\n"SYSHOSTINFOLONG":"",\n"SYSHOSTNAME":"sas",\n"SYSPROCESSID":"41DD607B5567F09E0000000000000000",\n"SYSPROCESSMODE":"Stored Program",\n"SYSPROCESSNAME":"",\n"SYSJOBID":"540520",\n"SYSSCPL":"LINUX",\n"SYSSITE":"0",\n"SYSTCPIPHOSTNAME":"https://sasjs.com",\n"SYSVLONG":"",\n"SYSWARNINGTEXT":"",\n"END_DTTM":"2022-06-21T16:46:40.740543",\n"AUTOEXEC":" ",\n"MEMSIZE":"0KB"}\n\`;\n\nconsole.log(_webout)\n`
-                      )
+                      code: encode(appinitJSContents)
                     },
                     {
                       name: 'appinit',
@@ -114,9 +130,7 @@ describe('sasjs cbd with server type SASJS', () => {
                     {
                       name: 'getdata.js',
                       type: 'file',
-                      code: encode(
-                        `// this file will take input and return a response\n\n_webout = \`\n{"SYSDATE":"21JUN22","SYSTIME":"17:59","springs":[{"LATITUDE":51.925,"LONGITUDE":-177.16,"NAME":"Fumaroles on Kanaga Island","AREA":"Adak","TYPE":"Hotspring","FARENHEIT":219,"CELSIUS":104},{"LATITUDE":52.042,"LONGITUDE":-176.108,"NAME":"Hot Springs on Great Sitkin Island","AREA":"Adak","TYPE":"Hotspring","FARENHEIT":210,"CELSIUS":99},{"LATITUDE":51.81,"LONGITUDE":-177.79,"NAME":"Hot Spring on Tanaga Island","AREA":"Adak","TYPE":"Hotspring","FARENHEIT":null,"CELSIUS":null},{"LATITUDE":51.97,"LONGITUDE":-176.61,"NAME":"Hot Springs on Adak Island","AREA":"Adak","TYPE":"Hotspring","FARENHEIT":154,"CELSIUS":68}],\n"SYSUSERID":"sasjsuser",\n"MF_GETUSER":"SASjs User",\n"_DEBUG":"",\n"_PROGRAM":"/Public/app/minimal-seed-app/services/common/getdata",\n"SYSCC":"0",\n"SYSERRORTEXT":"","SYSHOSTINFOLONG":"","SYSHOSTNAME":"sas",\n"SYSPROCESSID":"41DD607F9C7200B90000000000000000",\n"SYSPROCESSMODE":"Stored Program","SYSPROCESSNAME":"",\n"SYSJOBID":"542908",\n"SYSSCPL":"LINUX","SYSSITE":"0",\n"SYSTCPIPHOSTNAME":"https://sasjs.com",\n"SYSVLONG":"",\n"SYSWARNINGTEXT":"",\n"END_DTTM":"2022-06-21T17:59:14.515731",\n"AUTOEXEC":"","MEMSIZE":"0KB"}\n\`;\n`
-                      )
+                      code: encode(getdataJSContents)
                     },
                     {
                       name: 'getdata',
