259b24beea7795aa42cdb2f4ebffedcfbe5db8c1
Saad Jutt <ihsan.distranger@gmail.com>
2022-08-03 05:38:14 +0500

    fix(web): compiling web files having partial links to other files

--- src/commands/web/internal/modifyLinksInContent.ts
+++ src/commands/web/internal/modifyLinksInContent.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { AssetPathMap } from './createAssetServices'
 
 export const modifyLinksInContent = (
@@ -6,10 +7,29 @@ export const modifyLinksInContent = (
 ) => {
   let content = _content
   assetPathMap.forEach((pathEntry) => {
-    content = content.replace(
-      new RegExp(pathEntry.source, 'g'),
-      pathEntry.target
-    )
+    const { source, target } = pathEntry
+
+    const found = new RegExp(pathEntry.source, 'g').test(content)
+    if (found) {
+      content = content.replace(
+        new RegExp(pathEntry.source, 'g'),
+        pathEntry.target
+      )
+    } else {
+      const sourceWithoutExt = source.replace(
+        new RegExp(path.extname(source) + '$'),
+        '.'
+      )
+      const targetWithoutExt = target.replace(
+        new RegExp(path.extname(target) + '$'),
+        '.'
+      )
+
+      content = content.replace(
+        new RegExp(sourceWithoutExt, 'g'),
+        targetWithoutExt
+      )
+    }
   })
   return content
 }
