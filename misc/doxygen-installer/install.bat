b70a34be9fa27dbf4dd48ba36e96cec0f511bb94
Mihajlo Medjedovic <mihajlo.medjedovic@outlook.com>
2022-11-02 15:30:39 +0100

    ci: removed doxygen installer

--- misc/doxygen-installer/install.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-@echo Starting powershell installation script...
-PowerShell.exe -ExecutionPolicy Bypass -File script/installer_ps.ps1
-PAUSE
--- misc/doxygen-installer/uninstall.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-@echo Starting powershell uninstallation script...
-PowerShell.exe -ExecutionPolicy Bypass -File script/uninstaller_ps.ps1
-PAUSE
