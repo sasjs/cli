1fa293a10749ad1b4adc99a6f0e1b978b6e8e216
Yury Shkoda <shkoda.yuri@gmail.com>
2023-07-17 14:57:29 +0300

    fix(run): fixed for SASJS server type

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -590,10 +590,13 @@ export const getNodeModulePath = async (module: string): Promise<string> => {
 export const isSasJsServerInServerMode = async (target: Target) => {
   try {
     const res = await axios.get(`${target.serverUrl}/SASjsApi/info`)
+
     return res.data.mode === 'server'
   } catch (error) {
     const message = `An error occurred while fetching server info from ${target.serverUrl}/SASjsApi/info`
+
     displayError(error, message)
+
     throw new Error(message)
   }
 }
